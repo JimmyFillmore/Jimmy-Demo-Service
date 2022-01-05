8794e04539c78b441ba237bbc38445cc6fec7b2b
JimmyFillmore <69916335+JimmyFillmore@users.noreply.github.com>
2022-01-05 13:57:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,77 @@
 const express = require('express');
 const app = express();
-//const PORT = 8080;
 const PORT = process.env.PORT || 3000;
 
 const api_key = 'cIpBXj7m0OUnTzAYC229jhB7A0KigpKNVTmEXhuL3zw';
 const eleos_platform_key = '1ItgMN26pdJ87Vt7gNxAP2';
+const user_key = 'userkey'
 //const url = http://
 const fs = require('fs');
-const http = require('http');
+//const http = require('http');
 
 // Middleware to convert to json
 app.use( express.json() )
 
-app.get('', (req, res) => {
-    res.send('Hello World');
-});
+
 
 // make a function AuthenticateKey to check if the incoming Platform key against the stored key
 /*
 function AuthenticateKey () {
     let { EleosPlatformKey } = req.header.Eleos-Platform-Key
     if ( EleosPlatformKey != eleos_platform_key) {
-        res.status(401).send({ message: 'The request has an invalid key'})
+        res.status(401).send({ message: 'The request has an invalid key'});
     }
 };
 */
 
 /*
+function Authorization (req, res) {
+    const { incomingApiKey } = req.body;
+
+    if ( incomingApiKey != api_key ) {
+        res.status(418).send({ message: 'The request has an invalid api key'});
+    }
+    if (!incomingApiKey) {
+        res.status(418).send({ message: 'There is no api key'});
+    }
+    console.log ('Api key is ${incomingApiKey}')
+}
+*/
+
+app.get('', (req, res) => {
+    res.send('Hello World');
+});
+
+/*
 app.get('/authenticate/:token', (req, res) => {
     AuthenticateKey();
-    const { token } = res.body.api_token;
+    const { token } = req.body.api_token;
 });
 */
 
 app.get('/loads', (req, res) => {
  //   AuthenticateKey();
-    fs.readFile(__dirname + '/' + 'loads.json', 'utf8', (err,data) => {
-        res.end(data);
-    });
+ //   Authorization();
+    var myRequest = req.headers;
+    console.log(myRequest);
+    var myRequest2 = req.credentials;
+    console.log(myRequest2);
+    var myRequest3 = req.destination;
+    console.log(myRequest3);
+    var myRequest4 = req.referrer;
+    console.log(myRequest4);
+    var myRequest5 = req.url;
+    console.log(myRequest5);
+    if (myRequest.authorization == 'Token token='+user_key) {
+        const jsondata = require('./loads.json');
+        res.status(200).send(jsondata);
+    }
+
+    else {
+        res.status(401).send({'Error': '401 Unauthorized'})
+    }
+    
+    console.log (`Api key is ${myRequest.authorization}`);
 });
 
 /*
@@ -53,7 +87,6 @@ app.listen(
 );
 
 
-/*
 app.get('/tshirt', (req, res) => {
     res.status(200).send({
         tshirt:'SHIRT',
@@ -73,9 +106,6 @@ app.post('/tshirt/:id', (req, res) => {
         tshirt: `SHIRT with your ${logo} and ID of ${id}`,
     });
 });
-*/
-
-
 
 
 
